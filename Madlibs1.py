c34289dd90a88b3f79a1650ac00a7186dcf04554
Barbara <barbara.omukhango@mediapal.net>
2021-07-12 16:05:24 +0400

    changes on the madlibs story

--- Madlibs1.py
+++ Madlibs1.py
@@ -5,8 +5,12 @@ pluralnoun1= input(("Enter a plural noun"))
 noun2= input(("Enter a noun"))
 adverb= input(("Enter an adverb"))
 people= input(("Enter people"))
+place2= input(("Enter a place"))
+person= input(("Enter a person's name"))
+food= input(("Enter a type of food"))
 
 
 print ("It was a cold " + Day +" morning. My "+ noun1 + "and I went to the"
        + place1 + "On the way we saw two " + pluralnoun1 + "fighting. I stopped my car to intervene between them. As I held the" +
-       noun2 +" he turned to me" + adverb + "and slepped me. I became very annoyed. I knew I could't do anything to help so I went back to my car and called the" + people)
+       noun2 +" he turned to me" + adverb + "and slepped me. I became very annoyed. I knew I could't do anything to help so I went back to my car and called the" +
+       people + "Later I vistited the" + place2 + "where I met my" + person + "We ate "+ food + "even though it was midday." )
