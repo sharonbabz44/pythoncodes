64f0bd5b3b82478d393876e2f92eedfded6ad479
Barbara <barbara.omukhango@mediapal.net>
2021-07-12 16:10:13 +0400

    slight change

--- dict.py
+++ dict.py
@@ -1,11 +1,6 @@
-phone= int(input("Phone: "))
-digits_mapping={
-    "1" ="one"
-    "2" ="Two"
-    "3"="Three"
-    "4" = "Four"
+message = input (">")
+words=message.split("  ")
+        emojis = {
+            ":)" : "  "
+                        
 }
-
-for ch in phone:
-   output +=digits_mapping.get(ch, "!")
-   print (output)
