fe88398e05904026beca4ef4eaec96c38bf445f1
Barbara <barbara.omukhango@mediapal.net>
2021-07-15 16:00:48 +0400

    practice codes

---
+++ world.py
@@ -0,0 +1,13 @@
+msg= "Hello world"
+print (msg)
+
+x=2**5
+print (x)
+
+age=int(input("Enter your age"))
+print (age)
+
+
+fruits = ["banana" , "mangoes", "guava" ,"oranges"]
+print (fruits[2])
+print (fruits[1:])
