0e738ecd9e60359333f58583056e10876783312c
Barbara Sharon <sharonbabz@gmail.com>
2021-08-19 12:48:57 +0400

    Add files via upload

---
+++ ob.js
@@ -0,0 +1,11 @@
+let person = {
+    age: 30,
+    name: "John Smith",
+    FavColor: "Blue",
+    Country: "Algeria",
+    ShoeSize: "8",
+
+};
+console.log(person.name + " " + person.Country+ "  " + person.age);
+
+
